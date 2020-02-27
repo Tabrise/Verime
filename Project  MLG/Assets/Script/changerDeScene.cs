aea11cdc68e3a14842382ca977950b50c7c59ce7
Emilien Mattei <emilien_77400@hotmail.fr>
2020-02-27 14:52:04 +0100

    Scene de fin + ajustement

---
+++ Project  MLG/Assets/Script/changerDeScene.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class changerDeScene : MonoBehaviour
+{
+    public bool fin=false;
+    public GameObject perso;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        perso = GameObject.FindWithTag("perso");
+    }
+    void Start()
+    {
+        
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "perso")
+        {
+            fin = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fin == true)
+        {
+            SceneManager.LoadScene("sceneDeFin");
+        }
+    }
+}
