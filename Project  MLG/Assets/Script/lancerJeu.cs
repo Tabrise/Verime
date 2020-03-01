119d5d2ae09a967c67bf111f7422993ddf516ff3
Emilien Mattei <emilien_77400@hotmail.fr>
2020-03-01 14:27:31 +0100

    Shoot qui marche

--- Project  MLG/Assets/Script/lancerJeu.cs
+++ Project  MLG/Assets/Script/lancerJeu.cs
@@ -17,7 +17,6 @@ public class lancerJeu : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             SceneManager.LoadScene("SampleScene");
-          
         }
     }
 }
--- Project  MLG/Assets/Script/mouvementTire.cs
+++ Project  MLG/Assets/Script/mouvementTire.cs
@@ -9,6 +9,6 @@ public class mouvementTire : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        rb.velocity = (transform.right*-1) * vitesse;
     }
 }
